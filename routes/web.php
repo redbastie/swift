59ee184ae429ed2985ecb44bf4d243ddbdfb5a1a
redbastie <redbastie@icloud.com>
2020-10-13 14:25:29 -0400

    fix route bug

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,19 @@ use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('web')->group(function () {
-    foreach ((new Filesystem)->allFiles(app_path('Http/Livewire')) as $file) {
-        $className = 'App\\Http\\Livewire\\' . str_replace(['/', '.php'], ['\\', ''], $file->getRelativePathname());
-        $class = app($className);
+    $filesystem = new Filesystem;
+    $dir = app_path('Http/Livewire');
 
-        if ($class->routeUri) {
-            $route = Route::get($class->routeUri, $className);
-            if ($class->routeName) $route->name($class->routeName);
-            if ($class->routeMiddleware) $route->middleware($class->routeMiddleware);
+    if ($filesystem->exists($dir)) {
+        foreach ($filesystem->allFiles($dir) as $file) {
+            $className = 'App\\Http\\Livewire\\' . str_replace(['/', '.php'], ['\\', ''], $file->getRelativePathname());
+            $class = app($className);
+
+            if ($class->routeUri) {
+                $route = Route::get($class->routeUri, $className);
+                if ($class->routeName) $route->name($class->routeName);
+                if ($class->routeMiddleware) $route->middleware($class->routeMiddleware);
+            }
         }
     }
 });
